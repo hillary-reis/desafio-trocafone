84e17f7f38c14efb8efc6c69ce0cbc08c35f2765
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-10 18:31:27 -0300

    Desafio #2 CamelCase concluido

--- __tests__/camelCase.test.js
+++ __tests__/camelCase.test.js
@@ -1,10 +1,11 @@
 const camelCase = require('../src/camelCase');
+
 test("camelCase function", () => {
-	expect(camelCase("one Two")).toBe('oneTwo');
+	expect(camelCase("One Two")).toBe('oneTwo');
 	expect(camelCase("one!!Two")).toBe('oneTwo');
 	expect(camelCase("one-Two")).toBe('oneTwo');
 	expect(camelCase("one-Two Three")).toBe('oneTwoThree');
 	expect(camelCase("one-Two Three@")).toBe('oneTwoThree');
 	expect(camelCase("one-Two Three@")).toBe('oneTwoThree');
 	expect(camelCase("#hashtag for the win")).toBe('hashtagForTheWin');
-})
+});
--- src/camelCase.js
+++ src/camelCase.js
@@ -3,8 +3,40 @@
  * Ex: "an apple" deve retornar "anApple"
  * @see https://pt.wikipedia.org/wiki/CamelCase
  */
-function camelCase($inputString){
-	return 'to-do';
+function camelCase ($inputString) {
+	const inputStringCamelCase = $inputString;
+
+  // transforma a string em um array de strings
+  const arrayString = inputStringCamelCase.split(/(\W)/);
+
+  // transforma todos os elementos em minúsculo
+  arrayString.forEach( (eachString, index) => {
+    const element = eachString.toLowerCase();
+    
+    arrayString[index] = element;
+  });
+
+  // verificando se a string é uma palavra
+  const arrayFiltered = arrayString.filter((eachString) => /(\W)/.test(eachString) === false && eachString !== '');
+
+  // transforma do segundo elemento em diante o primeiro caracter em maiusculo
+  arrayFiltered.forEach( (eachString, index) => {
+    if (index !== 0) {
+      // primeira letra em maiusculo
+      const firstLetter = eachString.charAt(0).toUpperCase();
+
+      // resto da palavra em minusculo 
+      const restOfTheWord = eachString.substr(1);
+
+      // palavra toda
+      arrayFiltered[index] = firstLetter + restOfTheWord;
+    };
+  });
+
+  // concatena tudo em uma string só
+  const newString = arrayFiltered.join('');
+  
+  return newString;
 };
 
-module.exports = camelCase;
+module.exports = camelCase; 
