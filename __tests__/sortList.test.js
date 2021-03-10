c5f1157d00c958ac83066762776c9ebe02be4078
Hillary Reis <hillary.reis2002@gmail.com>
2021-03-10 14:48:25 -0300

    Desafio #01 SortList concluido

--- __tests__/sortList.test.js
+++ __tests__/sortList.test.js
@@ -3,4 +3,6 @@ const sortList = require('../src/sortList');
 test("sortList function", () => {
 	expect(sortList([2,1,5,8,3])).toStrictEqual([1,2,3,5,8]);
 	expect(sortList(["a","d","b","c","e"])).toStrictEqual(["a","b","c","d","e"]);
+	expect(sortList(["1","5","2","3","4"])).toStrictEqual(["1","2","3","4","5"]);
+	expect(sortList(['80', '9', '700', 40, 1, 5, 200])).toStrictEqual([1,5,'9',40,'80',200,'700']);
 });
--- src/sortList.js
+++ src/sortList.js
@@ -1,11 +1,17 @@
-
 /**
  * Recebe uma matriz e ordena ela pelo conteúdo.
  * Considere que virão apenas listas de números ou palavras.
- * @param array list 
- */
-function sortList(list){
-	return list;
-}
+ * @param array list
+
+ * referencias usadas:
+ * 01 - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+ * 02 - https://www.w3schools.com/jsref/jsref_sort.asp
+*/
+function sortList (list) {
+  list.sort(); // strings
+  list.sort( (a, b) => {return a - b} ); // numbers
+
+  return list;  
+};
 
 module.exports = sortList;
